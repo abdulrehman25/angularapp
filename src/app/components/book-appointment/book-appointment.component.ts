8ca903bbac219dfcee10ce09d91b92d13ee8ba00
anjali <anjalisingh@virtualemployee.com>
2023-03-30 19:12:22 +0530

    Updated  Book Appointment

--- src/app/components/book-appointment/book-appointment.component.ts
+++ src/app/components/book-appointment/book-appointment.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import * as $ from 'jquery';
 import { BookAppointment } from 'src/app/models/book-appointment';
 import { BookAppointmentService } from 'src/app/services/book-appointment.service';
 import { ToastrService } from 'ngx-toastr';
@@ -23,22 +22,23 @@ export class BookAppointmentComponent {
   appointment_number:"",
 
   };
-  userName: string = "jbond";
-  constructor(private router: Router , private bookAppointmentDataService:BookAppointmentService, private _toastr: ToastrService) {
-    console.log(this.objectForBookingAppointment);
-  }
+
+  showBookAppointmentPage:boolean = true;
+  addClass:string='';
+  divStyle:string ='';
+
+  constructor(private router: Router , private bookAppointmentDataService:BookAppointmentService, private _toastr: ToastrService) {  }
 
   ngOnInit() {
-    $('.book-appointment-page').show();
-    $('body').addClass('appointment-page');
+    this.showBookAppointmentPage = true;
   }
 
   addBookAppointmentData(appointmentData:NgForm){
-    this.bookAppointmentDataService.insertBookAppointmentData(this.objectForBookingAppointment).subscribe((res)=>{
-      console.log('result',res);
-
-      this._toastr.success('Data is saved Successfully !','Book Appointment data');
-      this.router.navigate(['/']);
+    this.bookAppointmentDataService.insertBookAppointmentData(this.objectForBookingAppointment).subscribe((res:any)=>{
+      if(res.status === 'success'){
+        this._toastr.success('Data is saved Successfully !','Book Appointment data');
+        this.router.navigate(['/']);
+      }
     })
   }
 
@@ -47,58 +47,13 @@ export class BookAppointmentComponent {
   }
 
   closeBookAppointment() {
-    //$('.close-book-appointment-page').click(function () {
-      $('.book-appointment-page').hide();
-      $('body').removeClass('appointment-page');
-    //});
+    this.showBookAppointmentPage = false;
     this.router.navigate(['/']);
   }
 
-  showStepTwo() {
-    $('.step2').show().css('z-index', '2');
+  showDiv(divClass:string,styleIndex:string){
+    this.addClass = divClass;
+    this.divStyle = 'z-index :'+styleIndex;
   }
 
-  showStepThree() {
-    $('.step3').show().css('z-index', '3');
-  }
-
-  showStepFour() {
-    $('.step4').show().css('z-index', '4');
-  }
-
-  showStepFive() {
-    $('.step5').show().css('z-index', '5');
-  }
-
-  showStepSix() {
-    $('.step6').show().css('z-index', '6');
-  }
-
-  showStepSeven() {
-    $('.step7').show().css('z-index', '7');
-  }
-
-  showStepEight() {
-    $('.step8').show().css('z-index', '8');
-  }
-
-  backToStepTwo() {
-    $('.step7').hide();
-  }
-
-  backToStepThree() {
-    $('.step6').hide();
-  }
-  backToStepFour() {
-    $('.step5').hide();
-  }
-  backToStepFive() {
-    $('.step4').hide();
-  }
-  backToStepSix() {
-    $('.step3').hide();
-  }
-  backToStepSeven() {
-    $('.step2').hide(); 
-  }
 }
