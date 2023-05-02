d73889e87490462c7ad3d16d639fb72eb20f95cf
satish verma <satishverma@virtualemployee.com>
2023-05-02 17:06:37 +0530

    flag icon and related work + rahul's changes

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -7,18 +7,40 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  showHide: boolean = false;
+  selectedLanguage = 'English';
+  selectedLanguageCode = 'gb';
+  selectedCountryCode = 'gb';
   languageList = [
-    { language: 'English', languageCode: 'en' },
-    { language: 'German', languageCode: 'de' },
-    { language: 'Arabic', languageCode: 'ar' },
-    { language: 'Russian', languageCode: 'ru' },
-    { language: 'Chinese', languageCode: 'zh' },
+    { language: 'English', languageCode: 'en', countryCode: 'gb' },
+    { language: 'German', languageCode: 'de', countryCode: 'de' },
+    { language: 'Arabic', languageCode: 'ar', countryCode: 'sa' },
+    { language: 'Russian', languageCode: 'ru', countryCode: 'ru' },
+    { language: 'Chinese', languageCode: 'zh', countryCode: 'cn' },
   ];
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'de', 'ar', 'ru', 'zh']);
     translate.setDefaultLang('en');
   }
-  switchLang(lang:string) {
-    this.translate.use(lang);
+
+  // switchLang(lang:string) {
+  //   this.translate.use(lang);
+  // }
+
+  showHideDropdown() {
+    this.showHide = !this.showHide;
+  }
+
+  switchLang(clickedLanguage: any) {
+    if (clickedLanguage) {
+      this.selectedLanguage = clickedLanguage.language;
+      this.selectedLanguageCode = clickedLanguage.languageCode;
+      this.selectedCountryCode = clickedLanguage.countryCode;
+      this.translate.use(clickedLanguage.languageCode);
+      this.showHide=false;
+    }
+  }
+  clickedOutside(){
+    this.showHide=false;
   }
 }
