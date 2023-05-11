776a7000268d3a767d3591e551f170d37e69b319
pravesh-ve <praveshyadav@virtualemployee.com>
2023-05-11 16:02:52 +0530

    dispaly user list and their report on admin section

--- src/app/components/admin/view-reports/view-reports.component.ts
+++ src/app/components/admin/view-reports/view-reports.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-view-reports',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-reports.component.css']
 })
 export class ViewReportsComponent {
+  p: number = 1;
+  sortDir = 0;
+  searchText: string = '';
+  userList: any;
+
+  constructor(private adminServices: AdminService, private _toastr: ToastrService, private ngxLoader: NgxUiLoaderService, private router: Router) {
+    this.getUserListData();
+    //this.sortArr('first_name');
+  }
+
+  getUserListData() {
+    this.ngxLoader.start();
+    this.adminServices.getUsersList().subscribe((res: any) => {
+      if (res.status == '200') {
+        this.userList = res.data;
+        this.ngxLoader.stop();
+      }
+    });
+  }
+
+  onSortClick() {
+    this.sortArr('first_name');
+  }
+
+  sortArr(colName: any) {
+    console.log(colName);
+    this.userList.sort((a: any, b: any) => {
+      a = a[colName].toLowerCase();
+      b = b[colName].toLowerCase();
+      return a.localeCompare(b) * this.sortDir;
+    });
+  }
+
 
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,21 +1,24 @@
-import { Injectable,Inject } from '@angular/core';
-import { HttpClient ,HttpHeaders} from '@angular/common/http';
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor(private httpClient:HttpClient,@Inject("BASE_URL") private baseUrl: string) { }
-  
+  constructor(private httpClient: HttpClient, @Inject("BASE_URL") private baseUrl: string) { }
+
   Api_Base = this.baseUrl;
 
-  getUsersList(){
+  getUsersList() {
     return this.httpClient.get(`${this.Api_Base}/users_list`);
   }
 
-  deleteUser(id:number){
+  deleteUser(id: number) {
     return this.httpClient.delete(`${this.Api_Base}/delete_user/${id}`);
   }
 
+  getUserReports(userId: number) {
+    return this.httpClient.get(`${this.Api_Base}/get_user_report/${userId}`);
+  }
 }
