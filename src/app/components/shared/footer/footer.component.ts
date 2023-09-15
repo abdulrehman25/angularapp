3b52d580c72c61d2df0fad3783f4b859e53c3db6
satish verma <satishverma@virtualemployee.com>
2023-09-15 20:23:14 +0530

    brain and spine sidebar hide on all radiology

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -8,49 +8,20 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 })
 export class FooterComponent {
   isHomePage: boolean = false;
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    // console.log("isHomePage", this.isHomePage);
-    // this.router.events.subscribe((event) => {
-    //   if (event instanceof NavigationEnd) {
-    //     // this.isHomePage = this.activatedRoute.snapshot.routeConfig?.path === 'home';
-    //     this.isHomePage = event.url === '/home';
-    //     console.log("isHomePage", this.isHomePage);
-    //   }
-    //   // console.log("isHomePage", this.isHomePage);
-
-    // });
-
-    
-  }
-
-  // ngOnInit() {
-  //   console.log("isHomePage", this.isHomePage);
-  //   this.router.events.subscribe((event) => {
-  //     if (event instanceof NavigationEnd) {
-  //       // Check if the current route's URL matches the home page URL
-  //       this.isHomePage = this.activatedRoute.snapshot.routeConfig?.path === 'home';
-  //       console.log("isHomePage", this.isHomePage);
-  //     }
-  //   });
-  // }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    // Check the current route when the component is initialized
     this.checkIfHomePage();
-
-    // Subscribe to route changes to update isHomePage
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        console.log('Current URL:', event.url);
-        // Check if the current route's URL matches the home page URL
+        // console.log('Current URL:', event.url);
         this.checkIfHomePage();
       }
     });
   }
 
   private checkIfHomePage() {
-    
     this.isHomePage = this.router.url === '/home';
-    console.log('isHomePage:', this.isHomePage);
+    // console.log('isHomePage:', this.isHomePage);
   }
 }
--- src/app/components/shared/radiology-sidebar/radiology-sidebar.component.ts
+++ src/app/components/shared/radiology-sidebar/radiology-sidebar.component.ts
@@ -1,11 +1,39 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-radiology-sidebar',
   templateUrl: './radiology-sidebar.component.html',
-  styleUrls: ['./radiology-sidebar.component.css']
+  styleUrls: ['./radiology-sidebar.component.css'],
 })
 export class RadiologySidebarComponent {
-  @Input() textFromParent='';
+  @Input() textFromParent = '';
+  isRadiologyPages: boolean = false;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  ngOnInit() {
+    this.checkIfRadiologyPages();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // console.log('Current URL:', event.url);
+        this.checkIfRadiologyPages();
+      }
+    });
+  }
 
+  private checkIfRadiologyPages() {
+    // this.isRadiologyPages = this.router.url === '/home';
+    if (
+      this.router.url === '/abdominalRadiology' ||
+      this.router.url === '/prostateRadiology' ||
+      this.router.url === '/interventionalRadiologyGuidance' ||
+      this.router.url === '/womensImaging' ||
+      this.router.url === '/musculoskeletalRadiology' ||
+      this.router.url === '/pediatricSportsInjuries' ||
+      this.router.url === '/pediatricRadiology' ||
+      this.router.url === '/oncologicalRadiology'
+    ) {
+      this.isRadiologyPages = true;
+      // console.log('radionlogy', this.isRadiologyPages);
+    }
+  }
 }
