6373d84f2d15ca6669445e503b6653ecd9618d3d
abdul rehman <abdulrehmankhokhar25@gmail.com>
2023-11-07 00:42:47 +0500

    api update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 const Config = {
     // BASE_URL: "http://127.0.0.1:8000/api"
-    BASE_URL: "https://radiologycheck.com/api"
+    BASE_URL: "https://backend.radiologycheck.com/api"
   };
 
   export function getBaseUrl() {
