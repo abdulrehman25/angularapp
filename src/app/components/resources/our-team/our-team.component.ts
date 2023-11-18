9fcadfd793710b3a8a0fa8c4737c603c34c78846
abdul rehman <abdulrehmankhokhar25@gmail.com>
2023-11-18 22:46:58 +0500

    typo fix

--- src/app/components/resources/our-team/our-team.component.ts
+++ src/app/components/resources/our-team/our-team.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-team.component.css']
 })
 export class OurTeamComponent {
-  OurTeamPageTitle = "Out Team At RadiologyCheck"
+  OurTeamPageTitle = "Our Team At RadiologyCheck"
 
-  
+
   pageTitle: string = 'Meet Our Team of Medical Professionals | Radiology Check';
   description: string = 'To provide personalized second opinions we have assembled a team of dedicated medical professionals. Click to learn more about the Radiology Check team!  ';
   keyword: string = 'Dr Ali Rahman, radiology team, Dr Sonke Bartling, specialist radiologists';
